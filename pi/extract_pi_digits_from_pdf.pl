f9ade4bf3403ba715d6724788c774d322cde9388
Doug Wegscheid <dwegscheid@sbcglobal.net>
2020-03-13 20:33:36 -0400

    Add extract_pi_digits_from_pdf.pl.

---
+++ pi/extract_pi_digits_from_pdf.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+
+sub readpi {
+ my ($fn) = @_;
+ open (F, $fn) || die $!;
+ my $on = 0;
+ my $pi = '';
+ while(<F>) {
+  s/^\x0c//;
+  chomp;
+  next if /^$/; 
+  if (/^(0?3\.?14159)/) {
+   $on = 1;
+   s/^0?3\.?14159/3.14159/;
+  } if ($_ !~ /^\d/) {
+   $on = 0;
+  }
+  $pi .= $_ if $on;
+ }
+ close F;
+ return $pi;
+}
+
+my $ref = readpi('pi1000000.txt');
+printf "length of ref = %d\n", length($ref);
+my $test = readpi ("pdftotext \"$ARGV[0]\" - |");
+#printf "pi = %s\n", $pi;
+printf "length of test = %d\n", length($test);
+
+my $same = 1;
+foreach my $i (0..length($test)-1) {
+ my $a = substr($ref,$i,1);
+ my $b = substr($test,$i,1);
+ if ($a ne $b) {
+  printf "difference in digit %d: %s <> %s\n", $i, $a, $b;
+  $same = 0;
+  break;
+ }
+}
+print "digits are correct\n" if $same;
